2fb100d79af6ac75cde2168c3f94604cdf1506ff
Gonzalo <garciacalvog@hotmail.com>
2024-10-15 16:18:47 -0300

    accessibity and seo lightHouse suggestions

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,8 +6,7 @@ import { useContext } from 'react'
 const NavBar = () => {
 
   const {language, handleLanguage} = useContext(LanguageContext)
-  console.log("language en NavBar: ", language)
-  console.log("path: ",window.location.pathname)
+
   return (
     <div className="navbar bg-[#0b5c7e] pt-0 pb-0 h-14 min-h-12" id="subir">
       <div className="navbar-start ">
@@ -17,13 +16,6 @@ const NavBar = () => {
           </label>
           <ul tabIndex={0} className="menu menu-sm dropdown-content z-[1] px-2 shadow bg-base-100 w-52">
             <li><NavLink to="/" className="text-lg" >Home</NavLink></li>
-            {/* <li><NavLink to="/cv" className="text-lg" >CV</NavLink></li> */}
-            {/* <li>
-              <NavLink to="/#contacts" >Contacto</NavLink>
-              <a href="/#contacts"  >
-              {language === 'es'? 'Contacto': 'Contact'}
-              </a>
-            </li> */}
             <li>
                 <a href="#contacts" className="p-2" >
                   {language === 'es'? 'Contacto': 'Contact'}
@@ -34,18 +26,20 @@ const NavBar = () => {
 
         <div className="dropdown hidden lg:block">
           <div className="menu menu-sm flex flex-row px-2 py-0">
-            <li><Link to="/" className="p-2" >Home</Link></li>
-            {/* <li><Link to="/cv" className="p-2">CV</Link></li> */}
+            <ul className="flex flex-row">
+              <li><Link to="/" className="p-2" >Home</Link></li>
               <li>
                 <a href="#contacts" className="p-2" >
                   {language === 'es'? 'Contacto': 'Contact'}
                 </a>
               </li>
+            </ul>
           </div>
         </div>
       </div>
       <div className="navbar-end px-4">
         <p>es</p>
+        <label className="hidden" htmlFor="my-toggle">Language</label>
         <input onClick={handleLanguage} type="checkbox" className="toggle toggle-accent mx-2" id="my-toggle" />
         <p>en</p>
       </div>
